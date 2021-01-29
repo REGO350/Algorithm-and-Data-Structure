04e639b93ad9382dfe44fc763db524aeeb79d7a3
REGO350 <riokanehiro228@gmail.com>
2021-01-29 14:47:07 +0900

    report15

--- 15/r1/r1.c
+++ 15/r1/r1.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main(void){
-  char c = 'P';
-  printf("%c\n",c+3);
-  printf("%c\n",c-2);
+  char c = 'A';
+  for(c=65; c<=90; c+=2)
+    printf("%c\n",c);
   return 0;
 }
